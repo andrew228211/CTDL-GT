72b4f442a5ee009495711f0d5cf01cf4bcfb4318
andrew228211 <andrew228211@gmail.com>
2022-03-09 14:50:06 +0700

    Create phantuthuk.cpp

---
+++ phantuthuk.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n,k,m;
+		cin>>n>>m>>k;
+		int a[n+m];
+		for(int i=0;i<n+m;i++)
+		{
+			cin>>a[i];
+		}
+		sort(a,a+n+m);
+		cout<<a[k-1]<<endl;
+	}
+}
