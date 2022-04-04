e74fca07c08a3cc95177c4d52e138e8311501233
andrew228211 <andrew228211@gmail.com>
2022-04-04 09:34:23 +0700

    Create BFS-dothivohuong.cpp

---
+++ BFS-dothivohuong.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n,m,k;
+int List[1005][1005];
+int chuaxet[1005];
+void bfs(int u)
+{
+	queue<int>dq;
+	dq.push(u);
+	chuaxet[u]=1;
+	while(!dq.empty())
+	{
+		int s=dq.front();
+		cout<<s<<" ";
+		dq.pop();
+		for(int i=1;i<=n;i++)
+		{
+			if(List[s][i]==1&&chuaxet[i]==0)
+			{
+				chuaxet[i]=1;
+				dq.push(i);
+			}
+		}
+	}
+}
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		cin>>n>>m>>k;
+		memset(List,0,sizeof(List));
+		memset(chuaxet,0,sizeof(chuaxet));
+		for(int i=1;i<=m;i++)
+		{
+			int u,v;
+			cin>>u>>v;
+			List[u][v]=1;
+			List[v][u]=1;
+		}
+		bfs(k);
+		cout<<endl;
+	}
+}
