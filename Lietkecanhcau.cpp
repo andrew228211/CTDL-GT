68fea6338e18aba819f71bb43ce6d10294dfd8a5
andrew228211 <andrew228211@gmail.com>
2022-04-04 09:35:45 +0700

    Create Lietkecanhcau.cpp

---
+++ Lietkecanhcau.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n,m;
+int List[1005][1005];
+int chuaxet[1005];
+int dfs(int u,int v)
+{
+	stack<int>st;
+	st.push(u);
+	while(!st.empty())
+	{
+		int s=st.top();
+		st.pop();
+		for(int i=1;i<=n;i++)
+		{
+			if(chuaxet[i]==0&&List[s][i]==1)
+			{
+				chuaxet[i]=1;
+				st.push(s);
+				st.push(i);
+				if(i==v)
+				{
+					return 1;
+				}
+				break;
+			}
+		}
+	}
+	return 0;
+}
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		cin>>n>>m;
+		queue<pair<int,int>>dq;
+		memset(List,0,sizeof(List));
+		for(int i=1;i<=m;i++)
+		{
+			int u,v;
+			cin>>u>>v;
+			dq.push({u,v});
+			List[u][v]=1;
+			List[v][u]=1;	
+		}		
+		pair<int,int>x;
+		while(!dq.empty())
+		{
+			x=dq.front();
+			dq.pop();
+			memset(chuaxet,0,sizeof(chuaxet));
+			List[x.first][x.second]=0;
+			List[x.second][x.first]=0;
+			if(dfs(x.first,x.second)==0)
+			{
+				cout<<x.first<<" "<<x.second<<" ";
+			}
+			List[x.first][x.second]=1;
+			List[x.second][x.first]=1;
+		}
+		cout<<endl;
+	}
+}
